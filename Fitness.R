39f315035e207c4cfe20811ffd175bcfec4e803e
Eric Son <ym.eric.son@gmail.com>
2019-11-17 23:39:31 -0600

    spatial join with shapefile
    latitude and longitude joined with community shapefile to assign neighborhoods to each fitness centers

--- Fitness.R
+++ Fitness.R
@@ -7,9 +7,7 @@ library(rgdal)
 library(gridExtra)
 library(grid)
 
-
-# Chicago Business Licenses - Current Active
-# https://bit.ly/2NP2U9K
+# ----------Chicago Business Licenses https://bit.ly/2NP2U9K----------
 setwd("C:/Users/ymeri/Documents/R/CPR-DataViz-Project/Data")
 businesses <- read.csv('Business_Licenses.csv')
 colnames(businesses) <- tolower(colnames(businesses))
@@ -21,21 +19,19 @@ na.omit(businesses, cols=c("ward", "long", "lat"))
 priv_fit <- businesses %>% 
   filter(str_detect(facility.type, "Fitness")) %>%
   filter(!str_detect(facility.type, 'Wrecking')) %>%
-  select(name, ward, facility.type, zip.code, long, lat)
-colnames(priv_fit)[4] <- "zip"
+  select(name, ward, zip.code, long, lat)
+colnames(priv_fit)[3] <- "zip"
 priv_fit$type <- 'Private'
 write.csv(priv_fit, file='Private_fitness_centers.csv')
 
-# Chicago Park District Parks
-# https://bit.ly/2OdU3P9
+# ----------Chicago Park District Parks https://bit.ly/2OdU3P9----------
 pub_parks <- read.csv('CPD_Parks.csv')
 colnames(pub_parks) <- tolower(colnames(pub_parks))
 pub_parks <- pub_parks %>%
   select(park_no, ward, park_class, zip) %>%
   filter()
 colnames(pub_parks)[1] <- "park.number"
-# Chicago Park Fitness Centers
-# https://bit.ly/2KpfRWP
+# ----------Chicago Park Fitness Centers https://bit.ly/2KpfRWP----------
 pub_fit <- read.csv('Chicago_Park_Fitness_Centers.csv')
 colnames(pub_fit) <- tolower(colnames(pub_fit))
 colnames(pub_fit)[1] <- "park"
@@ -44,14 +40,51 @@ pub_fit <- pub_fit %>%
   extract(location, c("lat", "long"), "\\(([^,]+), ([^)]+)\\)")
 # merge public parks with public fitness centers
 pub_fit <- merge(pub_fit, pub_parks, by='park.number') %>%
-  select(ward, park, facility.name, zip, long, lat, type) %>%
-  rename(name = park, facility.type = facility.name)
+  select(ward, park, zip, long, lat, type) %>%
+  rename(name = park) #, facility.type = facility.name)
 pub_fit$long <- as.numeric(as.character(pub_fit$long))
 pub_fit$lat <- as.numeric(as.character(pub_fit$lat))
 
+# ----------Chicago Community Areas ShapeFile https://bit.ly/37f1Btw----------
+il_spdf <- readOGR( 
+  dsn= getwd() , 
+  layer="geo_export_17e99094-f66e-4bf7-89b3-fa329ded2341",
+  verbose=FALSE)
+
 all_fit <- rbind(pub_fit, priv_fit)
-gyms_by_ward <- 
-  all_fit %>% group_by(ward) %>% count(ward)
+all_fit <- all_fit[!with(all_fit,is.na(long)& is.na(lat)),]
+
+#all_fit <- all_fit %>% drop_na(lat, long)
+coordinates(all_fit) <- ~long+lat
+proj4string(all_fit) <- proj4string(il_spdf)
+#plot(il_spdf) + points(all_fit)
+#over(all_fit, il_spdf)
+#cbind.data.frame(pts, country=over(pts, countries)$ADMIN)
+
+# Convert to sf-objects
+il_spdf.sf <- st_as_sf(il_spdf)
+all_fit.sf <- st_as_sf(all_fit)
+# Keep all "il_spdf.sf", sort by row.names(il_spdf.sf). Default overlay is "intersects".
+il_spdf_all_fit <- st_join(il_spdf.sf, all_fit.sf) 
+# Keeps all "all_fit.sf", sort by row.names(all_fit.sf)
+all_fit_il_spdf <- st_join(all_fit.sf, il_spdf.sf)
+# Convert back to Spatial*
+#il_spdf_all_fit <- as(il_spdf_all_fit, "Spatial")
+#all_fit_il_spdf <- as(all_fit_il_spdf, "Spatial")
+
+#gyms_by_com2 <- 
+#  il_spdf_all_fit %>% group_by(community) %>% count(community)
+
+gyms_by_com <- 
+  all_fit_il_spdf %>% group_by(community) %>% count(community)
+
+ggplot(gyms_by_com, aes(reorder(community, -n), n)) +
+  geom_col()
+
+
+
+
+
 
 # Chicago Community Areas
 il_spdf <- readOGR( 
@@ -59,8 +92,8 @@ il_spdf <- readOGR(
   #layer="geo_export_754fd6d2-6eab-484b-b590-75f1a6d0e041",
   layer="geo_export_17e99094-f66e-4bf7-89b3-fa329ded2341",
   verbose=FALSE)
-il_spdf <- merge(il_spdf, gyms_by_ward, by='ward')
-il_spdf@data[["n"]][is.na(il_spdf@data[["n"]])] <- 0
+#il_spdf <- merge(il_spdf, gyms_by_ward, by='ward')
+#il_spdf@data[["n"]][is.na(il_spdf@data[["n"]])] <- 0
 
 palette(colorRampPalette(c("white", "red"))(128))
 n_gym <- il_spdf@data[["n"]]
@@ -108,23 +141,6 @@ grid.arrange(tg, sg, p, nrow=3,
              heights=c(0.05, 0.05, 0.9))
 
 
-ggplot(gyms_by_ward, aes(reorder(ward, -n), n)) +
-  geom_col()
-
-ggplot(gyms_by_ward, aes(x=ward, y=n)) +
-  geom_col()
-
-library(grid); library(gridExtra)
-tg <- textGrob("Title Goes Here", gp=gpar(fontsize=30))
-sg <- textGrob("more subtle subtitle ", gp=gpar(fontsize=15, fontface=3L))
-margin <- unit(0.5)
-grid.newpage()
-grid.arrange(tg, sg,
-             heights = unit.c(grobHeight(tg) + 1.2*margin, 
-                              grobHeight(sg) + margin, 
-                              unit(1,"null")),
-             p1, p2, legend)
-
 # ggplot() +
 #   theme_bw() +
 #   geom_point(data=gyms, aes(x=long, y=lat)) +
@@ -140,7 +156,8 @@ grid.arrange(tg, sg,
 # cook county data - employment
 # obesity by zip code
 
-## gghiglight
+# For POINTS that fall within CA_counties, adds ATTRIBUTES, retains ALL pts if left=TRUE, otherwise uses inner_join
+isd_ca_co_pts <- st_join(il_spdf, left = FALSE, all_fit_long_lat["name"]) # join points
 
 
 
